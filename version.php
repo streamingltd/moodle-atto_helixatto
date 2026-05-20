bf2cdb17d9c11bdf35bdf09e84640f08b32c1b94
MEDIAL Moodle User <tim@medial.com>
2026-05-20 09:29:17 +0000

    Release version 9.1.000 (2026052002)

--- version.php
+++ version.php
@@ -24,14 +24,14 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025112401;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026052002;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112800;        // Requires this Moodle version.
 $plugin->component = 'atto_helixatto';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
 // Human readable version information.
-$plugin->release   = 'v9.0.003i';
+$plugin->release   = '9.1.000';
 
 $plugin->dependencies = [
-    'mod_helixmedia' => '2025112401',
+    'mod_helixmedia' => '2026052002',
 ];
 $plugin->supported = [401, 405];
